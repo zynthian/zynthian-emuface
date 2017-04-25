12d95fb6405f33310a3fe61d4eb90f464805ed96
jofemodo <fernando@zynthian.org>
2017-04-25 14:10:38 +0200

    Fix zynthian_gui wrapper for working with last changes.

--- zynthian_gui_emu.sh
+++ zynthian_gui_emu.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+export DISPLAY_WIDTH="320"
+export DISPLAY_HEIGHT="240"
+export ZYNTHIAN_AUBIONOTES=1
+export ZYNTHIAN_TOUCHOSC=1
+export ZYNTHIAN_WIRING_LAYOUT="EMULATOR"
+export ZYNTHIAN_LOG_LEVEL=10
+
 cd ../zynthian-ui
 exec ./zynthian_gui.py $1
