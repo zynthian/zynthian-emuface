811b0a493441fe09ca10bb7903f8895b15f6d825
jofemodo <fernando@zynthian.org>
2018-03-01 19:03:30 +0100

    Include config file and increase display size.

--- zynthian_gui_emu.sh
+++ zynthian_gui_emu.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-export DISPLAY_WIDTH="320"
-export DISPLAY_HEIGHT="240"
+source ../zynthian-sys/scripts/zynthian_envars.sh
+
+export DISPLAY_WIDTH="480"
+export DISPLAY_HEIGHT="320"
 export ZYNTHIAN_AUBIONOTES=1
 export ZYNTHIAN_TOUCHOSC=1
 export ZYNTHIAN_WIRING_LAYOUT="EMULATOR"
+export ZYNTHIAN_UI_FONT_FAMILY="Audiowide"
+export ZYNTHIAN_UI_FONT_SIZE="14"
+export ZYNTHIAN_UI_ENABLE_CURSOR="0"
 export ZYNTHIAN_LOG_LEVEL=10
 
 cd ../zynthian-ui
